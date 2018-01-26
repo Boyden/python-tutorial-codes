5ede7724990e44fa1dbfe6d09dba29c3baee4ad6
boyden <lee787359142@gmail.com>
2018-01-26 23:14:49 +0800

    Update opencv_python_toturial.py

--- opencv_python_toturial.py
+++ opencv_python_toturial.py
@@ -81,8 +81,36 @@ while(cap.isOpened()):
 	        break
 	else:
 	    break
-	    
+
 # Release everything if job is finished
 cap.release()
 out.release()
 cv2.destroyAllWindows()
+
+#1.2.3 Drawing Functions in OpenCV
+#functions:cv2.line(), cv2.circle() , cv2.rectangle(), cv2.ellipse(), cv2.putText() etc.
+
+#Drawing Line
+
+# Create a black image
+img = np.zeros((512,512,3), np.uint8)
+
+# Draw a diagonal blue line with thickness of 5 px
+img = cv2.line(img,(0,0),(511,511),(255,0,0),5)
+
+#Drawing Rectangle
+img = cv2.rectangle(img,(384,0),(510,128),(0,255,0),3)
+
+#Drawing Circle
+img = cv2.circle(img,(447,63), 63, (0,0,255), -1)
+
+#Drawing Ellipse
+img = cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
+
+#Drawing Polygon
+pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
+pts = pts.reshape((-1,1,2))
+img = cv2.polylines(img,[pts],True,(0,255,255))
+#cv2.polylines() can be used to draw multiple lines. Just create a list of all the lines you want to draw
+#and pass it to the function. All lines will be drawn individually. It is more better and faster way to draw a group of
+#lines than calling cv2.line() for each line.
