aa083881df7c9c5cc0fbf009ea76694ceacc04ec
boyden <lee787359142@gmail.com>
2018-01-27 21:09:05 +0800

    Update opencv_python_toturial.py

--- opencv_python_toturial.py
+++ opencv_python_toturial.py
@@ -91,6 +91,10 @@ cv2.destroyAllWindows()
 #functions:cv2.line(), cv2.circle() , cv2.rectangle(), cv2.ellipse(), cv2.putText() etc.
 
 #Drawing Line
+#lineType;
+#– LINE_8 (or omitted) - 8-connected line.
+#– LINE_4 - 4-connected line.
+#– LINE_AA - antialiased line.
 
 # Create a black image
 img = np.zeros((512,512,3), np.uint8)
@@ -114,3 +118,29 @@ img = cv2.polylines(img,[pts],True,(0,255,255))
 #cv2.polylines() can be used to draw multiple lines. Just create a list of all the lines you want to draw
 #and pass it to the function. All lines will be drawn individually. It is more better and faster way to draw a group of
 #lines than calling cv2.line() for each line.
+
+#Adding Text to Images
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
+
+#1.2.4 Mouse as a Paint-Brush
+
+events = [i for i in dir(cv2) if 'EVENT' in i]
+print(events)
+
+# mouse callback function
+def draw_circle(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(img,(x,y),100,(255,0,0),-1)
+
+# Create a black image, a window and bind the function to window
+img = np.zeros((512,512,3), np.uint8)
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_circle)
+
+while(1):
+    cv2.imshow('image',img)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+        
+cv2.destroyAllWindows()
