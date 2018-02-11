0a9cabe69dd62fd98d3b30c791d3e12981e85121
boyden <lee787359142@gmail.com>
2018-02-11 23:22:11 +0800

    update opencv_python_toturial.py

--- opencv_python_toturial.py
+++ opencv_python_toturial.py
@@ -765,3 +765,63 @@ blackhat = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, kernel)
 #https://web.archive.org/web/20160316070407/http://opencv-code.com/tutorials/hit-or-miss-transform-in-opencv/
 
 #Structuring Element
+
+# Rectangular Kernel
+cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
+
+# Elliptical Kernel
+cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+
+# Cross-shaped Kernel
+cv2.getStructuringElement(cv2.MORPH_CROSS,(5,5))
+
+#Image Gradients
+
+#1. Sobel and Scharr Derivatives
+
+#2. Laplacian Derivatives
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('dave.jpg',0)
+
+laplacian = cv2.Laplacian(img,cv2.CV_64F)
+sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
+
+plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
+plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+
+plt.show()
+
+#One Important Matter!
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('box.png',0)
+
+# Output dtype = cv2.CV_8U
+sobelx8u = cv2.Sobel(img,cv2.CV_8U,1,0,ksize=5)
+
+# Output dtype = cv2.CV_64F. Then take its absolute and convert to cv2.CV_8U
+sobelx64f = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+abs_sobel64f = np.absolute(sobelx64f)
+sobel_8u = np.uint8(abs_sobel64f)
+
+plt.subplot(1,3,1),plt.imshow(img,cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(1,3,2),plt.imshow(sobelx8u,cmap = 'gray')
+plt.title('Sobel CV_8U'), plt.xticks([]), plt.yticks([])
+plt.subplot(1,3,3),plt.imshow(sobel_8u,cmap = 'gray')
+plt.title('Sobel abs(CV_64F)'), plt.xticks([]), plt.yticks([])
+
+plt.show()
+
