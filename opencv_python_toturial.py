228a747eff4ce4be51fc88b95394cfe3753dcf7f
boyden <lee787359142@gmail.com>
2018-02-12 14:03:38 +0800

    update opencv_python_toturial.py

--- opencv_python_toturial.py
+++ opencv_python_toturial.py
@@ -841,3 +841,68 @@ plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 plt.show()
 
 #Image Pyramids
+img = cv2.imread('messi5.jpg')
+lower_reso = cv2.pyrDown(higher_reso)
+
+higher_reso2 = cv2.pyrUp(lower_reso)
+
+#Image Blending using Pyramids
+
+#1. Load the two images of apple and orange
+#2. Find the Gaussian Pyramids for apple and orange (in this particular example, number of levels is 6)
+#3. From Gaussian Pyramids, find their Laplacian Pyramids
+#4. Now join the left half of apple and right half of orange in each levels of Laplacian Pyramids
+#5. Finally from this joint image pyramids, reconstruct the original image.
+
+import cv2
+import numpy as np,sys
+
+A = cv2.imread('apple.jpg')
+B = cv2.imread('orange.jpg')
+
+# generate Gaussian pyramid for A
+G = A.copy()
+gpA = [G]
+for i in range(6):
+    G = cv2.pyrDown(G)
+    gpA.append(G)
+
+# generate Gaussian pyramid for B
+G = B.copy()
+gpB = [G]
+for i in range(6):
+    G = cv2.pyrDown(G)
+    gpB.append(G)
+
+# generate Laplacian Pyramid for A
+lpA = [gpA[5]]
+for i in range(5,0,-1):
+    GE = cv2.pyrUp(gpA[i])
+    L = cv2.subtract(gpA[i-1],GE)
+    lpA.append(L)
+
+# generate Laplacian Pyramid for B
+lpB = [gpB[5]]
+for i in range(5,0,-1):
+    GE = cv2.pyrUp(gpB[i])
+    L = cv2.subtract(gpB[i-1],GE)
+    lpB.append(L)
+
+# Now add left and right halves of images in each level
+LS = []
+for la,lb in zip(lpA,lpB):
+    rows,cols,dpt = la.shape
+    ls = np.hstack((la[:,0:cols/2], lb[:,cols/2:]))
+    LS.append(ls)
+
+# now reconstruct
+ls_ = LS[0]
+for i in range(1,6):
+    ls_ = cv2.pyrUp(ls_)
+    ls_ = cv2.add(ls_, LS[i])
+
+# image with direct connecting each half
+real = np.hstack((A[:,:cols/2],B[:,cols/2:]))
+
+cv2.imwrite('Pyramid_blending2.jpg',ls_)
+cv2.imwrite('Direct_blending.jpg',real)
