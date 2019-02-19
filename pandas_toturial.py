3c5ba71d27317ef7c7271c26d41ff6f4bbbc2474
boyden <lee787359142@gmail.com>
2019-02-19 16:57:42 +0800

    update codes

--- pandas_toturial.py
+++ pandas_toturial.py
@@ -135,3 +135,8 @@ obj4
 
 obj.index = ['Bob', 'Steve', 'Jeff', 'Ryan']
 obj
+
+data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada'],
+        'year': [2000, 2001, 2002, 2001, 2002],
+        'pop': [1.5, 1.7, 3.6, 2.4, 2.9]}
+frame = DataFrame(data)
