20cf1c6c57778fd36fda858cd4b19b261f4e40f1
boyden <lee787359142@gmail.com>
2019-02-20 14:24:25 +0800

    update pandas codes

--- pandas_toturial.py
+++ pandas_toturial.py
@@ -140,3 +140,189 @@ data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada'],
         'year': [2000, 2001, 2002, 2001, 2002],
         'pop': [1.5, 1.7, 3.6, 2.4, 2.9]}
 frame = DataFrame(data)
+
+DataFrame(data, columns=['year', 'state', 'pop'])
+
+
+# In[ ]:
+
+
+frame2 = DataFrame(data, columns=['year', 'state', 'pop', 'debt'],
+                   index=['one', 'two', 'three', 'four', 'five'])
+frame2
+
+frame2.columns
+
+
+# In[ ]:
+
+
+frame2['state']
+
+
+# In[ ]:
+
+
+frame2.year
+
+
+# In[ ]:
+
+
+# frame2.ix['three']
+frame2.loc['three']
+
+# In[ ]:
+
+
+frame2['debt'] = 16.5
+frame2
+
+
+# In[ ]:
+
+
+frame2['debt'] = np.arange(5.)
+frame2
+
+val = Series([-1.2, -1.5, -1.7], index=['two', 'four', 'five'])
+frame2['debt'] = val
+frame2
+
+frame2['eastern'] = frame2.state == 'Ohio'
+frame2
+
+del frame2['eastern']
+frame2.columns
+
+
+# In[ ]:
+
+
+pop = {'Nevada': {2001: 2.4, 2002: 2.9},
+       'Ohio': {2000: 1.5, 2001: 1.7, 2002: 3.6}}
+
+
+# In[ ]:
+
+
+frame3 = DataFrame(pop)
+frame3
+
+
+# In[ ]:
+
+
+frame3.T
+
+
+# In[ ]:
+
+
+DataFrame(pop, index=[2001, 2002, 2003])
+
+
+# In[ ]:
+
+
+pdata = {'Ohio': frame3['Ohio'][:-1],
+         'Nevada': frame3['Nevada'][:2]}
+DataFrame(pdata)
+
+
+# In[ ]:
+
+
+frame3.index.name = 'year'; frame3.columns.name = 'state'
+frame3
+
+
+# In[ ]:
+
+
+frame3.values
+
+
+# In[ ]:
+
+
+frame2.values
+
+
+# ### Index objects
+
+# In[ ]:
+
+
+obj = Series(range(3), index=['a', 'b', 'c'])
+index = obj.index
+index
+
+
+# In[ ]:
+
+
+index[1:]
+
+
+# In[ ]:
+
+
+index[1] = 'd'
+
+
+# In[ ]:
+
+
+index = pd.Index(np.arange(3))
+obj2 = Series([1.5, -2.5, 0], index=index)
+obj2.index is index
+
+
+# In[ ]:
+
+
+frame3
+
+
+# In[ ]:
+
+
+'Ohio' in frame3.columns
+
+
+# In[ ]:
+
+
+2003 in frame3.index
+
+
+# ## Essential functionality
+
+# ### Reindexing
+
+# In[ ]:
+
+
+obj = Series([4.5, 7.2, -5.3, 3.6], index=['d', 'b', 'a', 'c'])
+obj
+
+
+# In[ ]:
+
+
+obj2 = obj.reindex(['a', 'b', 'c', 'd', 'e'])
+obj2
+
+
+# In[ ]:
+
+
+obj.reindex(['a', 'b', 'c', 'd', 'e'], fill_value=0)
+
+
+# In[ ]:
+
+
+obj3 = Series(['blue', 'purple', 'yellow'], index=[0, 2, 4])
+obj3.reindex(range(6), method='ffill')
